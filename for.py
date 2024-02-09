758def37d57e81669743546eeafeb165c3791788
xiaoooooowen <xiaowenzhang2004@outlook.com>
2024-02-09 15:24:26 +0800

    缩进

--- for.py
+++ for.py
@@ -2,4 +2,4 @@ magics = ['alice','jack','malong']
 for magic in magics:
     print(f"{magic.title()},you are very nb.")
     print(f"I can't wait to see your next trick,{magic.title()}.\n")
-    
+print(f"you are all very good!")
